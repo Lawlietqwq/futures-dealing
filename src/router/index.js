ecba378719553fdb56c893c938f88163f219ce2a
lawliet <1848351377@qq.com>
2022-06-24 21:07:45 +0800

    layout

---
+++ src/router/index.js
@@ -0,0 +1,124 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+/* Layout */
+import Layout from '@/layout'
+
+export const constantRoutes = [
+    {
+      path: '/redirect',
+      component: Layout,
+      hidden: true,
+      children: [
+        {
+          path: '/redirect/:path(.*)',
+          component: () => import('@/views/redirect/index')
+        }
+      ]
+    },
+    // {
+    //   path: '/login',
+    //   component: () => import('@/views/login/index'),
+    //   hidden: true
+    // },
+    {
+      path: '/register',
+      component: () => import('@/views/register/index'),
+      hidden: true
+    }, 
+    {
+      path: '/forgetPass',
+      component: () => import('@/views/forgetPass/index'),
+      hidden: true
+    },
+    
+    {
+      path: '/auth-redirect',
+      component: () => import('@/views/login/auth-redirect'),
+      hidden: true
+    },
+    {
+      path: '/404',
+      component: () => import('@/views/error-page/404'),
+      hidden: true
+    },
+    {
+      path: '/401',
+      component: () => import('@/views/error-page/401'),
+      hidden: true
+    },
+    {
+      path: '/',
+      component: Layout,
+      children: [
+        {
+          path: '',
+          component: () => import('@/views/case/index'),
+          name: 'index',
+          meta: { title: '期货交易系统', icon: 'lock', noCache: true }
+        }
+      ]
+    },
+  
+  ]
+  
+  /**
+   * asyncRoutes
+   * the routes that need to be dynamically loaded based on user roles
+   */
+  export const asyncRoutes = [
+  
+  
+    {
+      path: '/caseView',
+      component: () => import('@/views/caseView/index'),
+      name: 'caseView',
+    },
+  
+    {
+      path: '/caseDetail',
+      component: () => import('@/views/caseDetail/index'),
+      name: 'caseDetail',
+    },
+  
+    // {
+    //   path: '/icon',
+    //   component: Layout,
+    //   children: [
+    //     {
+    //       path: 'index',
+    //       component: () => import('@/views/icons/index'),
+    //       name: 'Icons',
+    //       meta: { title: 'Icons', icon: 'icon', noCache: true }
+    //     }
+    //   ]
+    // },
+  
+    /** when your routing map is too long, you can split it into small modules **/
+    // componentsRouter,
+    // chartsRouter,
+    // nestedRouter,
+    // tableRouter,
+  
+    // 404 page must be placed at the end !!!
+    { path: '*', redirect: '/404', hidden: true }
+  ]
+  
+  const createRouter = () => new Router({
+    // mode: 'history', // require service support
+    scrollBehavior: () => ({ y: 0 }),
+    routes: constantRoutes
+  })
+  
+  const router = createRouter()
+  
+  // Detail see: https://github.com/vuejs/vue-router/issues/1234#issuecomment-357941465
+  export function resetRouter() {
+    const newRouter = createRouter()
+    router.matcher = newRouter.matcher // reset router
+  }
+  
+  export default router
+  
---
+++ src/views/case/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <p>
+        Vue2/3+vue-router+vuex+element-ui
+    </p>
+  </div>
+</template>
